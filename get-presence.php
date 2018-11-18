6bc758f4b5f107ac759647a67e849efadd57ce56
David Lounsbrough <david.lounsbrough@gmail.com>
2018-11-17 19:56:55 -0600

    Adding endpoint

---
+++ get-presence.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__FILE__).'/classes/authentication.php';
+$authentication = new Authentication();
+
+$jsonBody = $authentication->getRequestJSON(file_get_contents('php://input'));
+$authentication->authenticateRequest($jsonBody);
+
+$directory = new DirectoryIterator(dirname(__FILE__).'/control-files');
+foreach ($directory as $fileinfo) {
+    if (!$fileinfo->isDot()) {
+        $file = $fileinfo->getBasename('.json');
+        $people[$file] = json_decode(file_get_contents("control-files/$file.json"));
+    }
+}
+
+echo json_encode($people);
+?>
