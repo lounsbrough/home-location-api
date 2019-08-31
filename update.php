1bcfb262d8f1dfe5a941439ca2d2da34347b0a2d
David Lounsbrough <david.lounsbrough@gmail.com>
2019-08-31 13:55:15 -0500

    Add check for undetermined humidity

--- update.php
+++ update.php
@@ -66,7 +66,8 @@ if ($locationTriggersEnabled)
     if ($requestParser->location == 'home' && $requestParser->status == 'arrived') {
         $currentTemperature = $nest->getTemperature();
         $currentHumidity = $nest->getHumidity();
-        if ($currentHumidity < 35 || $currentHumidity > 60) {
+
+        if ($currentHumidity != '' && ($currentHumidity < 35 || $currentHumidity > 60)) {
             $deviceName = ucwords(strtolower($requestParser->person))." - Phone";
             $noteBody = "Temperature: ".round($currentTemperature, 1)."° Humidity: $currentHumidity%";
             $pushbullet->pushNote($deviceName, "Humidity Alert", $noteBody);
